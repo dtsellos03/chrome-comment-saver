69089535c6d9e5c36d8f0588f50fc8a218bcce36
dtsellos03 <dtsellos03@gmail.com>
2017-07-22 21:34:07 -0700

    Added main functionality of inserting a button with event listener

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,41 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+var selectors = ['.Ct', '.comment-renderer-text-content', 'yt-formatted-string.content-text'];
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
 
-	}
-	}, 10);
-});
+// adds button
+var addButton = function(comment) {
+
+    parent = comment.parentElement;
+
+    next = parent.nextSibling,
+    button = document.createElement("button");
+    button.className = "button";
+    text = document.createTextNode("save");
+    button.appendChild(text);
+    parent.appendChild(button);
+
+    comment.classList.add('buttoned');
+
+}
+
+// build the full selector string
+var commentSelectorString = selectors.map(function(sel) {
+  return sel + ':not(.buttoned)';
+}).join(", ");
+
+// every 100 milliseconds, apply button function to unmodified comments
+setInterval(function() {
+  document.querySelectorAll(commentSelectorString).forEach(addButton);
+}, 100);
+
+setInterval(function() {
+  var classname = document.getElementsByClassName("button");
+  for (i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', handler, false);
+  }
+}, 100);
+
+function handler() {
+	console.log("CLICKED")
+  console.log(this.parentElement.children)
+  console.log(this.parentElement.children[0].innerHTML)
+}
