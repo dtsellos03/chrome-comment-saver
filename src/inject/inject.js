462c97dedb6fd10af77f7526dfcc9ac6fd442902
dtsellos03 <dtsellos03@gmail.com>
2017-07-29 21:13:45 -0700

    Added feature to change CSS of button once saved

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,6 +6,8 @@ const authorImageUrlSel = '.yt-thumb-clip';
 const authorNameSel = '.comment-author-text';
 const commentTextSel = '.comment-renderer-text-content';
 const commentUrlSel = '.comment-renderer-time';
+const likeSel = '.comment-renderer-like-count';
+
 
 // adds button
 var addButton = function(comment) {
@@ -14,7 +16,7 @@ var addButton = function(comment) {
 
   next = parent.nextSibling,
     button = document.createElement("button");
-  button.className = "button";
+  button.className = "buttonInj";
   text = document.createTextNode("save");
   button.appendChild(text);
   parent.appendChild(button);
@@ -34,7 +36,7 @@ setInterval(function() {
 }, 100);
 
 setInterval(function() {
-  var classname = document.getElementsByClassName("button");
+  var classname = document.getElementsByClassName("buttonInj");
   for (i = 0; i < classname.length; i++) {
     classname[i].addEventListener('click', handler, false);
   }
@@ -42,21 +44,31 @@ setInterval(function() {
 
 function handler() {
 
+  var that = this;
+
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
   var overallCommentParent = getXthParentElement(this, levelsOfIndentation);
   var comment = {}
   comment.text = overallCommentParent.querySelector(commentTextSel).innerHTML;
   comment.url = overallCommentParent.querySelector(commentUrlSel).children[0].getAttribute("href");
   comment.author = overallCommentParent.querySelector(authorNameSel).innerHTML
   comment.authorImg = overallCommentParent.querySelector(authorImageUrlSel).children[0].getAttribute("src");
+  comment.likeCount = overallCommentParent.querySelector(likeSel).innerHTML;
+  comment.dateSaved = date;
   comment.videoTitle = document.getElementById('eow-title').getAttribute("title");
   comment.videoID = getVideoID();
 
-  chrome.storage.sync.get('comments', function(commentArray) {
+  chrome.storage.local.get('comments', function(commentArray) {
 
-    commentArray.comments.push(comment)
-    chrome.storage.sync.set({
+    commentArray.comments.unshift(comment)
+    chrome.storage.local.set({
       comments: commentArray.comments
     }, function() {
+      that.innerHTML = "Saved";
+      that.classList.add("savedButton");
+      that.disabled = true;
       console.log("Sucess!")
     });
   });
